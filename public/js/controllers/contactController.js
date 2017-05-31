7826b5592b76263f18f1a2597df02aad09ce6eeb
maitabe <martushb@gmail.com>
2017-05-31 22:19:01 +0300

    hide/show add contact

--- public/js/controllers/contactController.js
+++ public/js/controllers/contactController.js
@@ -1,5 +1,6 @@
 app.controller('contactCtrl', ['$scope', 'contactService', '$stateParams', function($scope, contactService, $stateParams) {
 
+
 	//find contact by id
 	console.log($stateParams.id);
 	$scope.contact = contactService.findById($stateParams.id);
